83acd99180100b2814605131e62401c07463181a
Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
2021-01-13 16:35:22 +0100

    Initial commit.

---
+++ app.py
@@ -0,0 +1,98 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+import json
+from charts import *
+
+
+# Data retrieval and preprocessing
+
+VACCINI_SUMMARY_URL = "https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/vaccini-summary-latest.csv"
+SOMM_VACC_LATEST = "https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-latest.csv"
+REGION_CODE_NAME = {
+    "ABR": "abruzzo",
+    "BAS": "basilicata",
+    "CAL": "calabria",
+    "CAM": "campania",
+    "EMR": "emilia-romagna",
+    "FVG": "friuli venezia giulia",
+    "LAZ": "lazio",
+    "LIG": "liguria",
+    "LOM": "lombardia",
+    "MAR": "marche",
+    "MOL": "molise",
+    "PAB": "provincia autonoma di bolzano",
+    "PAT": "provincia autonoma di trento",
+    "PIE": "piemonte",
+    "PUG": "puglia",
+    "SAR": "sardegna",
+    "SIC": "sicilia",
+    "TOS": "toscana",
+    "UMB": "umbria",
+    "VDA": "valle d'aosta",
+    "VEN": "veneto",
+    "TRA": "trentino-alto adige/sudtirol",
+}
+
+# Retrieve data from open repository
+# Credit: TODO
+vax_summary = pd.read_csv(VACCINI_SUMMARY_URL)
+vax_summary["regione"] = vax_summary.area.apply(lambda x: REGION_CODE_NAME[x])
+
+somm_vacc_latest = pd.read_csv(SOMM_VACC_LATEST)
+somm_vacc_latest["total"] = (
+    somm_vacc_latest.sesso_maschile + somm_vacc_latest.sesso_femminile
+)
+
+# build trentino aldo adige's data summing data from
+# - Provincia Autonoma di Trento
+# - Provincia Autonoma di Bolzano
+pat = vax_summary[vax_summary.area == "PAT"]
+pab = vax_summary[vax_summary.area == "PAB"]
+tra = {
+    "area": "TRA",
+    "dosi_somministrate": pat.dosi_somministrate.values[0]
+    + pab.dosi_somministrate.values[0],
+    "dosi_consegnate": pat.dosi_consegnate.values[0] + pab.dosi_consegnate.values[0],
+    "percentuale_somministrazione": 100
+    * (pat.dosi_somministrate.values[0] + pab.dosi_somministrate.values[0])
+    / (pat.dosi_consegnate.values[0] + pab.dosi_consegnate.values[0]),
+    "ultimo_aggiornamento": pat.ultimo_aggiornamento.values[0],
+    "regione": "trentino-alto adige/sudtirol",
+}
+vax_summary = vax_summary.append(tra, ignore_index=True)
+
+# Load region borders from GeoJSON
+# Credit: TODO
+with open("italy-regions.json") as fp:
+    regions = json.load(fp)
+names = [f["properties"]["name"] for f in regions["features"]]
+
+# Figures
+
+ic = ItalyChoropleth(vax_summary, regions)
+scs = ScatterConsegnateSomministrate(vax_summary)
+bbs = BarBySex(somm_vacc_latest)
+pbs = PieSex(somm_vacc_latest)
+bbo = BarByOccupation(somm_vacc_latest)
+
+# DASH APP
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(
+    children=[
+        html.H1(children="ItalVax"),
+        html.Div(children="""Da un'idea molto semplice."""),
+        dcc.Graph(id="italy-choropleth", figure=ic.get_figure()),
+        dcc.Graph(id="scatter-consegnate-somministrate", figure=scs.get_figure()),
+        dcc.Graph(id="bar-by-sex", figure=bbs.get_figure()),
+        dcc.Graph(id="pie-by-sex", figure=pbs.get_figure()),
+        dcc.Graph(id="bar-by-occupation", figure=bbo.get_figure()),
+    ]
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ charts.py
@@ -0,0 +1,81 @@
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+
+
+class ItalyChoropleth:
+    def __init__(self, vax_summary, regions):
+        self.vax_summary = vax_summary
+        self.regions = regions
+
+    def get_figure(self):
+        fig = px.choropleth(
+            self.vax_summary,
+            geojson=self.regions,
+            locations="regione",
+            color="percentuale_somministrazione",
+            # color_continuous_scale="Viridis",
+            range_color=(0, 100),
+            featureidkey="properties.name",
+            projection="mercator",
+        )
+        fig.update_geos(fitbounds="locations", visible=False)
+        fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+        return fig
+
+
+class ScatterConsegnateSomministrate:
+    def __init__(self, vax_summary):
+        self.vax_summary = vax_summary
+
+    def get_figure(self):
+        return px.scatter(
+            self.vax_summary,
+            x="dosi_consegnate",
+            y="dosi_somministrate",
+            size="percentuale_somministrazione",
+            color="percentuale_somministrazione",
+        )
+
+
+class BarBySex:
+    def __init__(self, somm_vacc_latest):
+        self.somm_vacc_latest = somm_vacc_latest
+
+    def get_figure(self):
+        return px.bar(
+            self.somm_vacc_latest.groupby("area").sum().sort_values("total"),
+            y=["sesso_femminile", "sesso_maschile"],
+        )
+
+
+class PieSex:
+    def __init__(self, somm_vacc_latest):
+        self.somm_vacc_latest = somm_vacc_latest
+
+    def get_figure(self):
+        return go.Figure(
+            data=[
+                go.Pie(
+                    labels=["Donne", "Uomini"],
+                    values=self.somm_vacc_latest[["sesso_femminile", "sesso_maschile"]]
+                    .sum()
+                    .values,
+                )
+            ]
+        )
+
+
+class BarByOccupation:
+    def __init__(self, somm_vacc_latest):
+        self.somm_vacc_latest = somm_vacc_latest
+
+    def get_figure(self):
+        return px.bar(
+            self.somm_vacc_latest.groupby("area").sum().sort_values("total"),
+            y=[
+                "categoria_operatori_sanitari_sociosanitari",
+                "categoria_ospiti_rsa",
+                "categoria_personale_non_sanitario",
+            ],
+        )
